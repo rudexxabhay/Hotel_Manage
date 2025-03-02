ff800c20811df019ae45acba122a34fa7f095dd3
rudexxabhay <ks8817112@gmail.com>
2025-03-02 06:55:04 -0800

    Fixed vite config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),],
-<<<<<<< HEAD
-=======
-  base: "/",
->>>>>>> 208632d0a01401ae1d10e37efd1892b1d8cd1c23
 })
